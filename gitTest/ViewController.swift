7bb1af9fd65e478dd63bc62ab4d5d0f867c726ff
Timecapsule120 <nakazono.timecapsule@gmail.com>
2017-05-03 18:38:36 +0900

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Timecapsule120 on 2017/05/03.
+//  Copyright © 2017年 timecapsule20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
